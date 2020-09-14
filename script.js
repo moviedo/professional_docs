9e67e1560faf678536921f675b34c3388b817dce
Mauro Oviedo <mauro.oviedo@protonmail.com>
2020-09-13 20:38:04 -0400

    Updated pdf script to use nunjucks & pdf-puppeteer.

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
-const Prince = require("prince")
-const util 	 = require("util")
-const fs 		 = require('fs');
-const path 	 = require('path');
+const fs = require('fs');
+
+const nunjucks = require('nunjucks');
+const convertHTMLToPDF = require('pdf-puppeteer');
 
 
 const resumeDir = './resumes';
 const distDir = './dist';
 
 function buildPdf() {
-	fs.readdir('./resumes', (err, files) => {
-		if (!err) {
-			files.forEach(fileName => {
-				Prince()
-					.inputs(path.join(resumeDir, fileName))
-					.output(path.join(distDir, `${fileName.split('.')[0]}.pdf`))
-					.execute()
-					.then(function () {
-					}, function (error) {
-						console.log("ERROR: ", util.inspect(error));
-					});
-			});
-		}
-	});
+	const html = nunjucks.render(`${resumeDir}/frontend.html`, require('./data/frontend.json'));
+	convertHTMLToPDF(html, pdf => fs.writeFileSync(`${distDir}/frontend.pdf`, pdf), {margin: {
+		top: '20mm',
+		right: '20mm',
+		bottom: '20mm',
+		left: '20mm',
+	}});
+
+// fs.readFile(`${resumeDir}/frontend.html`, 'utf8', (err,template) => {
+//   if (!err) {
+// 		const html = nunjucks.render(template);
+// 		convertHTMLToPDF(html, pdf => fs.writeFileSync(`${distDir}/frontend.pdf`, pdf), {margin: {
+// 			top: '20mm',
+// 			right: '20mm',
+// 			bottom: '20mm',
+// 			left: '20mm',
+// 		}});
+//   }
+//   console.log(err);
+// });
+
 }
 
 fs.mkdir(distDir, err => {
