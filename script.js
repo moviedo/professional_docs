8da7eff5c5bf119cb404e4af08d73189505a3f59
Mauro Oviedo <mauro@ixlayer.com>
2021-08-30 14:54:04 +0200

    refactor(script): use promises, lint code

--- script.js
+++ script.js
@@ -1,37 +1,46 @@
-const fs = require('fs');
 
-const nunjucks = require('nunjucks');
-const convertHTMLToPDF = require('pdf-puppeteer');
+const { mkdir, readdir, writeFile } = require('fs/promises')
+const nunjucks = require('nunjucks')
+const convertHTMLToPDF = require('pdf-puppeteer')
 
+const dataDir = './data'
+const distDir = './dist'
+const PuppeteerOptions = Object.freeze({
+  margin: {
+    top: '20mm',
+    right: '20mm',
+    bottom: '20mm',
+    left: '20mm'
+  }
+})
 
-const dataDir = './data';
-const distDir = './dist';
-const options = {
-	margin: {
-		top: '20mm',
-		right: '20mm',
-		bottom: '20mm',
-		left: '20mm',
-	}
-};
+async function buildPdf () {
+  try {
+    const files = await readdir(dataDir)
+    files.forEach(fileName => {
+      const name = fileName.split('.')[0]
+      const html = nunjucks.render('resume.html', require(`./data/${fileName}`))
 
-function buildPdf() {
-	fs.readdir(dataDir, (err, files) => {
-		if (!err) {
-			files.forEach(fileName => {
-				const name = fileName.split('.')[0];
-				const html = nunjucks.render('resume.html', require(`./data/${fileName}`));
-
-				convertHTMLToPDF(html, pdf => fs.writeFileSync(`${distDir}/${name}.pdf`, pdf), options);
-			});
-		} else
-			console.log('ERROR: ', err);
-	});
+      convertHTMLToPDF(
+        html,
+        async (pdf) => {
+          await writeFile(`${distDir}/${name}.pdf`, pdf)
+        },
+        PuppeteerOptions
+      )
+    })
+  } catch (e) {
+    console.log(`Error in buildPdf: ${e}`)
+  }
 }
 
-fs.mkdir(distDir, err => {
-	if (!err || err.errno === -17) {
-		buildPdf();
-	} else
-		console.log(err);
-});
+async function main () {
+  try {
+    await mkdir(distDir, { recursive: true })
+    buildPdf()
+  } catch (e) {
+    console.log(`Error in main: ${e}`)
+  }
+}
+
+main()
