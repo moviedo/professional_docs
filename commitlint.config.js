09e4dacbfdd5fe82ff2038faf23d80407fdbb0d6
Mauro Oviedo <mauro@ixlayer.com>
2021-08-30 13:09:34 +0200

    build: add commitlint, update node/npm, & remove yarn

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+
+  /*
+   * Any rules defined here will override rules from @commitlint/config-conventional
+   */
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'style',
+        'test',
+      ],
+    ],
+  },
+}
